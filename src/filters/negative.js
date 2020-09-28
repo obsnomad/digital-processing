4b0e3931f5693591bd47f1338825baafd992ead8
Aleksandr Katelnikov <aleksandr.katelnikov@x5.ru>
2020-09-28 23:17:58 +0300

    Negative filter comments

--- src/filters/negative.js
+++ src/filters/negative.js
@@ -5,9 +5,11 @@ export default {
         const {width, height} = context.canvas
         const data = context.getImageData(0, 0, width, height)
         let maxLuminosity = 0;
+        // Получение максимального значения яркости во всех цветовых составляющих RGB
         for (let i = 0; i < data.data.length; i += 4) {
             maxLuminosity = Math.max(maxLuminosity, data.data[i], data.data[i + 1], data.data[i + 2])
         }
+        // Преобразование в негатив
         for (let i = 0; i < data.data.length; i += 4) {
             data.data[i] = maxLuminosity - data.data[i]
             data.data[i + 1] = maxLuminosity - data.data[i + 1]
