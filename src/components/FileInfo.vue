5cf80f52bb000ca8bb0ce9f604c29870c1c3d386
Aleksandr Katelnikov <aleksandr.katelnikov@x5.ru>
2020-12-01 19:34:55 +0300

    Huffman compression

--- src/components/FileInfo.vue
+++ src/components/FileInfo.vue
@@ -3,6 +3,8 @@
         <p>Исходный размер: {{ size }}</p>
         <p>Размер после сжатия RLE: {{ sizeRLE }}</p>
         <p>Размер после распаковки RLE: {{ sizeAfterRLE }}</p>
+        <p>Размер после сжатия методом Хаффмана: {{ sizeHuffman }}</p>
+        <p>Размер после распаковки методом Хаффмана: {{ sizeAfterHuffman }}</p>
     </div>
 </template>
 
@@ -16,6 +18,9 @@ export default {
         context: null,
         data: null,
         dataRLE: null,
+        dataHuffman: null,
+        treeHuffman: null,
+        treeHuffmanSize: null,
         counterSize: 4,
     }),
     computed: {
@@ -41,6 +46,21 @@ export default {
                         uncompressedData.push(...color)
                     }
                 })
+            })
+            return displaySize(uncompressedData.length)
+        },
+        sizeHuffman() {
+            return this.dataHuffman ? displaySize((this.dataHuffman.reduce(
+                (accumulator, row) => accumulator + row.length, 0
+            ) + this.treeHuffmanSize) / 8) : 'н/д'
+        },
+        sizeAfterHuffman() {
+            if (!this.dataHuffman) {
+                return 'н/д'
+            }
+            const uncompressedData = [];
+            this.dataHuffman.forEach((value) => {
+                uncompressedData.push(...this.findNodeValue(value))
             })
             return displaySize(uncompressedData.length)
         },
@@ -76,7 +96,75 @@ export default {
                 }
             }
             this.dataRLE = dataRLE
-        }
+        },
+        encodeHuffman() {
+            const data = Array.from(this.data)
+            let frequencies = {}
+            let {treeHuffman} = this
+            treeHuffman = []
+            for (let i = 0; i < data.length; i += 4) {
+                let index = '';
+                for (let j = 0; j < 4; j++) {
+                    index += data[i + j].toString(16).padStart(2, '0')
+                }
+                if (!frequencies[index]) {
+                    frequencies[index] = 0
+                }
+                frequencies[index]++
+            }
+            treeHuffman = this.getSortedEntries(frequencies)
+            let nodesAmount = 1;
+            while (treeHuffman.length > 2) {
+                const first = treeHuffman.shift()
+                const second = treeHuffman.shift()
+                treeHuffman.unshift([[first, second], first[1] + second[1]]);
+                treeHuffman.sort(([, a], [, b]) => a - b)
+                nodesAmount++
+            }
+            let codes = {}
+            this.treeHuffman = this.getCompressedTree(treeHuffman)
+            this.treeHuffmanSize = 9 * 2 * 4 * nodesAmount
+            this.getTreeCodes(this.treeHuffman, codes)
+            this.dataHuffman = []
+            for (let i = 0; i < data.length; i += 4) {
+                let index = '';
+                for (let j = 0; j < 4; j++) {
+                    index += data[i + j].toString(16).padStart(2, '0')
+                }
+                this.dataHuffman.push(codes[index])
+            }
+        },
+        getSortedEntries(obj) {
+            return Object.entries(obj).sort(([, a], [, b]) => a - b)
+        },
+        getCompressedTree([[first], [second]]) {
+            return [
+                typeof first === 'object' ? this.getCompressedTree(first) : first,
+                typeof second === 'object' ? this.getCompressedTree(second) : second,
+            ]
+        },
+        getTreeCodes(data, codes, code = '') {
+            for (let i in data) {
+                const currentCode = code + i
+                const value = data[i]
+                if (typeof value === 'string') {
+                    codes[value] = currentCode
+                }
+                else {
+                    this.getTreeCodes(value, codes, currentCode)
+                }
+            }
+        },
+        findNodeValue(path) {
+            let tree = {...this.treeHuffman}
+            for (let dir of path) {
+                if (!dir || typeof tree === 'string') {
+                    break
+                }
+                tree = tree[dir]
+            }
+            return tree.match(/../g).map((value) => parseInt(value, 16))
+        },
     },
     watch: {
         file(file) {
@@ -90,6 +178,7 @@ export default {
                     context.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight)
                     this.data = getImageData(this.context).data
                     this.encodeRLE()
+                    this.encodeHuffman()
                 }
             }
         },
